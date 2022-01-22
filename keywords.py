beb03ba323420162d6e46760da42e09b8e6f2d80
tada20001 <tada20001@gmail.com>
2022-01-22 11:54:57 +0900

    edit7

--- keywords.py
+++ keywords.py
@@ -12,18 +12,19 @@ from wordcloud import WordCloud
 st.write("""
 # WordCloud Generator
 """)
-expander_bar1 = st.expander("About")
+expander_bar1 = st.expander("About", expanded=True)
 expander_bar1.markdown("""
 * NTIS의 한글 키워드 정보를 이용해 워드 클라우드를 그립니다.
 * 입력 엑셀파일에 "한글키워드" 필드가 반드시 포함되어 있어야 합니다.
-* 아래의 에러문구는 파일을 업로드하면 사라집니다. (나중에 해결!!)
+* 파일을 업로드하면 그래프와 wordcloud 메뉴들이 화면에 나타납니다.
+* 사용방법
 
     * 첫째, 왼쪽 Side bar에서 단어갯수 지정
     * 둘째, 왼쪽 Side bar에서 제외할 단어 지정
     * 셋째, 메인에서 플롯으로 확인
     * 위의 순서를 반복하면서 단어 갯수와 제외할 단어 결정
 
-* 옵션으로 유사한 단어를 하나로 수정하는 것도 가능합니다. 
+* 옵션으로 유사한 단어들을 하나로 수정하는 것도 가능합니다. 
     * 단, 두개 이상의 단어를 수정할 경우, sidebar의 예와 같이 미세먼지를 첫번째로 놓고 초미세먼지를 두번째로 놓으면 알고리즘은 순차적으로 발생하기 때문에 초미세먼지를 완전히 걸러내지 못합니다. 이 경우에는 초미세먼지를 먼저 쓰고 두번째로 미세먼지를 써야 제대로 걸러집니다.
     * 여러개 단어를 수정하고 싶으면 엑셀에서 작업하는 것을 강력 추천합니다. 여기서는 간단한 단어만 변경.. 
 """)
@@ -43,33 +44,26 @@ st.sidebar.header('User Input Features')
 # 0. 파일 열기
 # Collects user input features into dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your excel file", type=["xlsx"])
-input_df = pd.read_excel(uploaded_file)
-st.sidebar.write("""
-***
-""")
+try: 
+    input_df = pd.read_excel(uploaded_file)
+except:
+    input_df = None
+st.sidebar.write("""***""")
 
 # 1. 순위 지정
 st.sidebar.subheader('단어 갯수 설정')
 rank = st.sidebar.slider('ex) 최다빈도 단어 1위부터 30위까지 보여주려면 30으로 지정', 10, 100, 30)
-
-st.sidebar.write("""
-***
-""")
+st.sidebar.write("""***""")
 
 # 2. 카운팅 안할 단어 지정
-st.sidebar.write("""
-### 제외할 단어 지정
-""")
+st.sidebar.write("""### 제외할 단어 지정""")
 stop_words = '및', '환경', '오염', '|', '및', '초', '성', '기술', '시스템', \
               '구조', '의', '중심으로', '관한', '대한', ':', '기반', '효과', \
               '개발', '에', '과', '연구', '적', '방법', '위한','변화', '미치는',\
               '활용', '-', '난', '프로', '미세먼지'
 
 words = st.sidebar.text_area("제외할 단어를 따옴표로 감싸 아래와 같이 입력", stop_words, height=250)
-
-st.sidebar.write("""
-***
-""")
+st.sidebar.write("""***""")
 
 st.sidebar.write('단어를 수정할 수 있습니다.')
 input1 = st.sidebar.text_input('변경할 단어1', '미세먼지')
@@ -105,54 +99,48 @@ def tokenizer(df, words):
             result.append(w)
     return result
 
-tokens = tokenizer(input_df, words)
-#st.write(words)
+if input_df is not None:
+    tokens = tokenizer(input_df, words)
+    #st.write(words)
 
 # 4. 그래프 그리기
-st.subheader('1. Display a plot')
-filename = 'ex1.png'
-figsize = (12, 6)
-expander_bar2 = st.expander("About")
-expander_bar2.markdown("""
-        * 그래프에 표시된 단어들을 확인하면서 의미없는 단어는 제외해 주세요
-""")
-
-if st.button('그래프 표시'):
-    st.write('단어별 빈도수 (' + str(rank) + ' Rank 까지)' )
-    f, ax = plt.subplots(figsize=figsize)
-    common_words = Text(tokens)
-    ax = common_words.plot(rank)
-    plt.xlabel('빈도가 높은 단어')
-    plt.xticks(fontsize =10)
-    st.pyplot(f)
-
-st.write("""
-***
-""")
-
-# 5. Download dataframe
-st.subheader('2. Download a table')
+if input_df is not None:
+    st.subheader('1. Display a plot')
+    filename = 'ex1.png'
+    figsize = (12, 6)
+    expander_bar2 = st.expander("About")
+    expander_bar2.markdown("""
+        * 그래프에 표시된 단어들을 확인하면서 의미없는 단어는 제외해 주세요.""")
+    if st.button('그래프 표시'):
+        st.write('단어별 빈도수 (' + str(rank) + ' Rank 까지)' )
+        f, ax = plt.subplots(figsize=figsize)
+        common_words = Text(tokens)
+        ax = common_words.plot(rank)
+        plt.xlabel('빈도가 높은 단어')
+        plt.xticks(fontsize =10)
+        st.pyplot(f)
+    st.sidebar.write("""***""")
 
 # 가장 많이 사용된 단어 카운팅
 @st.cache
 def get_most_common_words(tokens, rank):
     fd_names = FreqDist(tokens)
     return fd_names.most_common(rank), fd_names
-
-most_words, fd_names = get_most_common_words(tokens, rank)
-most_df = pd.DataFrame(most_words, columns=['단어', '빈도'])
-
-st.dataframe(most_df.head(10))
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False, encoding='utf-8-sig')
     b64 = base64.b64encode(csv.encode(encoding='utf-8-sig')).decode(encoding='utf-8-sig')
     href = f'<a href="data:file/csv;base64,{b64}" download="common_words.csv">Download CSV File</a>'
     return href
-st.markdown(filedownload(most_df), unsafe_allow_html=True)
-st.write("""
-***
-""")
+
+if input_df is not None:
+    # 5. Download dataframe
+    st.subheader('2. Download a table') 
+    most_words, fd_names = get_most_common_words(tokens, rank)
+    most_df = pd.DataFrame(most_words, columns=['단어', '빈도'])
+    st.dataframe(most_df.head(10))
+    st.markdown(filedownload(most_df), unsafe_allow_html=True)
+    st.write("""***""")
 
 ## 6. Display wordcloud
 def draw_wordcloud(fd):
@@ -161,14 +149,16 @@ def draw_wordcloud(fd):
     plt.axis('off')
     plt.show()
 
-st.subheader('3. Create a wordcloud image')
-if st.button('워드클라우드 표시'):
-    draw_wordcloud(fd_names)
-    st.pyplot(plt)
+if input_df is not None:
+    st.subheader('3. Create a wordcloud image')
+    if st.button('워드클라우드 표시'):
+        draw_wordcloud(fd_names)
+        st.pyplot(plt)
 
-st.write("""
-***
-""")
+    st.write("""
+    ***
+    """)
+
 
 ## 7. Display wordcloud
 def draw_wordcloud(fd):
@@ -177,16 +167,19 @@ def draw_wordcloud(fd):
     plt.axis('off')
     plt.show()
 
-fd_names_upper = {}
-for i in range(rank):
-    fd_names_upper[most_words[i][0]] = most_words[i][1]
 
-st.subheader('4. Top ranking image')
-if st.button('워드클라우드(상위랭크만) 표시'):
-    draw_wordcloud(fd_names_upper)
-    st.pyplot(plt)
 
-st.write("""
-***
-""")
+if input_df is not None:
+    fd_names_upper = {}
+    for i in range(rank):
+        fd_names_upper[most_words[i][0]] = most_words[i][1]
+
+    st.subheader('4. Top ranking image')
+    if st.button('워드클라우드(상위랭크만) 표시'):
+        draw_wordcloud(fd_names_upper)
+        st.pyplot(plt)
+
+    st.write("""
+    ***
+    """)
 
